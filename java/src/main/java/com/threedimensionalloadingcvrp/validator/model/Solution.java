438115896ee8670ad3eb945aff7588c56f8a6c2c
CorinnaKrebs <39536359+CorinnaKrebs@users.noreply.github.com>
2021-07-29 16:44:09 +0200

    Update Solution.java

--- java/src/main/java/com/threedimensionalloadingcvrp/validator/model/Solution.java
+++ java/src/main/java/com/threedimensionalloadingcvrp/validator/model/Solution.java
@@ -16,6 +16,9 @@ public class Solution {
 
     /** Total Travel Distance */
     private double     total_travel_distance;
+    
+    /** Total Travel Time */
+    private double     total_travel_time;
 
     /** Feasibility of the Solution (1: feasible) */
     private boolean    feasible;
